fae69485bacb6761a26d7feb749ef3a8c6f82382
Naufal Aditya Dirgandhavi <naufalkps@gmail.com>
2019-04-19 10:27:24 +0700

    regex udah oke, tinggal ditambahin

--- backend.py
+++ backend.py
@@ -3,6 +3,7 @@ import re
 from tesaurus import *
 from BM import *
 from kmp import *
+from regex import *
 
 stopword = StopWordRemoverFactory().create_stop_word_remover()
 
@@ -14,13 +15,13 @@ for line in open('pertanyaan.txt').readlines():
     while (line[j] != ' '):
         j+=1
     while (line[i] != '?'):
-        i+=1
-    #pertanyaan.append(((line[j+1:i+2].lower())))    
+        i+=1   
     pertanyaan.append(stopword.remove((line[j+1:i+2].lower())))
     jawaban.append(line[i+2:len(line)-1])
 
 while(True):
     query = input("Masukkan Query : ").lower()
+    print()
     query = stopword.remove(query)
     querylist = query.split(' ')
     synonymList = []
@@ -36,21 +37,23 @@ while(True):
         sentenceList = newList
 
     pertanyaanRekomendasi = []
-    maxi = 0
+    #maxi = 0
+
     for sentence in sentenceList:
         sentence = (' '.join(sentence))
-        #print(sentence)
+        regexQuery = regex(sentence)
         i = 0
         for p in pertanyaan:
             found = False
-            kmp = find_occurrences(p, sentence)/len(sentence)
-            bm = bmMatch(p, sentence)/len(sentence)
+            rgx = re.search(regexQuery, p)
+            kmp = find_occurrences(p, sentence)/len(p)
+            bm = bmMatch(p, sentence)/len(p)
             #kecocokan diatas 90%
-            maxi = max(maxi,max(kmp,bm))
-            if(kmp>=0.8 or bm >= 0.8):
+            #maxi = max(maxi,max(kmp,bm))
+            if(kmp>=0.9 or bm >= 0.9 or rgx):
                 found = True
                 break
-            elif(kmp>=0.6 or bm >= 0.6):
+            elif(kmp>=0.6 or bm >= 0.6 and len(pertanyaanRekomendasi)<3):
                 pertanyaanRekomendasi.append(p)
             #kalo gaada 90%, sementara diadd dulu ke pertanyaan yang diatas 60%
             i+=1
@@ -69,7 +72,9 @@ while(True):
             print(x)
     else:
         print("Aku tidak mengerti maksud pertanyaanmu. ")
-        print("Ngomong-ngomong, tahukah kamu bahwa kita bernafas kira-kira 23.000 kali setiap harinya! Kalau aku engga sih hehe")
+        print("Ngomong-ngomong, tahukah kamu bahwa kita bernafas kira-kira 23.000 kali setiap harinya!")
+        print("Ya kalau aku engga sih hehe")
         #fakta gaje lah wkwk
+        print()
         print("Silahkan coba tanya aku lagi untuk fakta lainnya!")
     print()
